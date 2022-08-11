fcd7f4327e47b3aa1208f384e6920f08df6057b4
Hilquias Ferreira Melo <hilquiasfmelo@hotmail.com>
2022-08-11 09:49:32 -0300

    Páginas e rotas - Layout de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
+import { Router } from './Router'
 import { defaultTheme } from './styles/themes/_default'
 import { GlobalStyle } from './styles/_global'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="success" />
-      <Button variant="danger" />
-      <Button />
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
